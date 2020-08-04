f6a85005b10c0e2135d8c3b919fe7cb177db5ae7
kaz101 <kaz101130@gmail.com>
2020-08-03 21:58:09 -0400

    fix

--- transcript_analyzer/views.py
+++ transcript_analyzer/views.py
@@ -162,7 +162,7 @@ def get_picture(request,person, series):
                 images.append(i.get_attribute('src'))
         driver.close()
         try:
-            link = images[70]
+            link = images[len(images)-1]
         except:
             link = None
         return link
